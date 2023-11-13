7812fc3b59b77df85a8865df7cb4011eb7c41aba
LeggenDarie <131869432+LeggenDarie@users.noreply.github.com>
2023-11-13 18:28:55 +0100

    Delete Project directory

--- Project/app/controllers/application_controller.rb
+++
@@ -1,6 +0,0 @@
-class ApplicationController < ActionController::Base
-    def destroy
-        sign_out(current_user) # This method is provided by Devise
-        redirect_to root_path, notice: 'You have been successfully signed out.'
-      end
-end
--- Project/app/controllers/support_controller.rb
+++
@@ -1,7 +0,0 @@
-class SupportController < ApplicationController
-    before_action :authenticate_user!, except: [:index, :show]
-
-    def index
-        # cacca pipì pupù
-    end
-end
