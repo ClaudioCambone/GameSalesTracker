e50b1d937fc5fd2111373ffef11a9f2ee4f565fe
LeggenDarie <darie.1952989@studenti.uniroma1.it>
2023-11-18 12:23:09 +0100

    Messaggio descrittivo delle modifiche

--- Project/app/controllers/games_controller.rb
+++ Project/app/controllers/games_controller.rb
@@ -1,60 +1,81 @@
 class GamesController < ApplicationController
   layout 'navbar_layout'
+  before_action :set_api_key
   before_action :authenticate_user!, except: [:index, :show]
 
   #http_basic_authenticate_with name: "user", password: "user", except: [:index, :show]
 
+
   def index
-    @games = Game.all
-  end
-
-  def show 
-    @game = Game.find(params[:id])
-  end
-
-  def new 
-    @game = Game.new
-  end
-
-  def create 
-    @game = Game.new(game_params)
-    if @game.save
-      redirect_to @game
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def edit
-    @game = Game.find(params[:id])
+    @games = [] # O qualsiasi altra logica per ottenere i giochi desiderati
   end
   
-  def update
-    @game = Game.find(params[:id])
-    if @game.update(game_params)
-      redirect_to @game
+  def search
+    if params[:search_query].present?
+      @search_query = params[:search_query]
+      @games = search_games
     else
-      render :new, status: :unprocessable_entity
+      @games = []
     end
   end
 
-  def destroy
-    @game = Game.find(params[:id])
-    @game.destroy
+  def details
+    game_plain = params[:plain]
 
-    redirect_to root_path, status: :see_other
+    begin
+      @game_details = get_game_details([game_plain])
+    rescue StandardError => e
+      flash[:error] = "Errore nel recupero dei dettagli del gioco: #{e.message}"
+      @game_details = nil
+    end
   end
 
-  def search
-    steam_api = SteamApi.new
-    @games = steam_api.search(params[:game_name])
-    render :search_results
+  private
+
+  def set_api_key
+    @api_key = 'b14274e8092bc14e227b32e4b66c2903bf4419c9'
   end
 
-  private 
-  def game_params
-    params.require(:game).permit(:title, :body, :status)
+  def search_games
+    url = "https://api.isthereanydeal.com/v02/search/search/?key=#{@api_key}&q=#{params[:search_query]}&limit=20&strict=0"
+
+    begin
+      response = RestClient.get(url)
+      parsed_response = JSON.parse(response)
+      game_data = parsed_response['data']['results']
+
+      if game_data.present?
+        return game_data
+      else
+        return []
+      end
+    rescue RestClient::ExceptionWithResponse => e
+      puts "Errore nella richiesta: #{e.response}"
+      return []
+    rescue => e
+      puts "Errore: #{e.message}"
+      return []
+    end
   end
 
+  def get_game_details(game_plains)
+    details_url = "https://api.isthereanydeal.com/v01/game/info/?key=#{@api_key}&plains=#{game_plains.join(',')}"
+    
+    response = RestClient.get(details_url)
+    parsed_response = JSON.parse(response)
+    game_details = parsed_response['data']
+
+    if game_details.present?
+      return game_details
+    else
+      return []
+    end
+  rescue RestClient::ExceptionWithResponse => e
+    puts "Errore nella richiesta di dettagli dei giochi: #{e.response}"
+    return []
+  rescue => e
+    puts "Errore nella richiesta di dettagli dei giochi: #{e.message}"
+    return []
+  end
 end
 
--- Project/config/routes.rb
+++ Project/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/auth/facebook/callback', to: 'users/omniauth_callbacks#facebook'
   root "games#index"
+  get 'games/search', to: 'games#search', as: :search_games
+  get 'games/details/:plain', to: 'games#details', as: :details_game
   get '/support', to: 'support#index'
   get 'profile/profile_page'
 
@@ -12,10 +14,6 @@ Rails.application.routes.draw do
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end  
-
-  get 'games/search', to: 'games#search', as: :search_games
-
- 
 
   resources :games do
     resources :comments
