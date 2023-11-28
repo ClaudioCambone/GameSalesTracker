c4240a1edc643cd33fb996c7a13da40ff41b1af7
karisinpain <126679790+karisinpain@users.noreply.github.com>
2023-11-28 15:51:25 +0100

    login/sign up layout
    added a new layout (with bootstrap) to the pages of login, sign-up, forgot password and resend confirmation instructions

--- Project/config/routes.rb
+++ Project/config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   root "games#index"
-<<<<<<< HEAD
-    get '/user', to: 'user#index'
-  get 'games/search', to: 'games#search', as: :search_games
-  get 'games/details/:plain', to: 'games#details', as: :details_game
-  get 'games/store_lowest_prices', to: 'games#store_lowest_prices'
- #  get '/auth/facebook/callback', to: 'users/omniauth_callbacks#facebook'
-
-=======
->>>>>>> 20f798f1f89b11f88d8ec960c5d2c22338e06360
 
   get '/user', to: 'user#index'
   get '/support', to: 'support#index'
@@ -24,11 +15,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     confirmations: 'users/confirmations'
   }
-<<<<<<< HEAD
-  
-=======
-
->>>>>>> 20f798f1f89b11f88d8ec960c5d2c22338e06360
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end  
