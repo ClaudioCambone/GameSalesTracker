9eb637cf3f9fc2a3bd9616ea7a34ce59ca89bd48
ClaudioCamb <cambone.1933719@studenti.uniroma1.it>
2024-03-18 19:25:17 +0100

    Added a test for User edit data

--- Project/spec/features/user_spec.rb
+++ Project/spec/features/user_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature "Authentication", type: :feature do
-  ########## TEST N. 1 TRYING TO CREATE, CONFIRM AND SIGN IN A USER ##########
-  scenario "User can register an account and sign in" do
+  ######### TEST N. 1 TRYING TO CREATE, CONFIRM AND SIGN IN A USER ##########
+  scenario "User register an account and sign in" do
     visit "http://localhost:3000/users/sign_up"
 
 
     fill_in "Username", with: "Tartosso"
     fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password"
+    fill_in "typePasswordConfirmation", with: "password"
+
     click_button "Sign up"
 
     # Check for the confirmation message
-    expect(page).to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account")
 
     # Retrieve the confirmation token from the database
     user = User.find_by(email: "test@example.com")
     confirmation_token = user.confirmation_token
+    expect(page).to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account")
+
 
     # Simulate visiting the confirmation link
     visit "http://localhost:3000/users/confirmation?confirmation_token=" + confirmation_token
@@ -33,7 +36,36 @@ RSpec.feature "Authentication", type: :feature do
     # Check for successful sign-in
     expect(page).to have_content("Signed in successfully.")
   end
+  ########## TEST N. 2 TRYING TO MODIFY A USER NAME PASSWORD AND AVATAR ##########
+  scenario "User can modify credentials and sign in with updated credentials" do
+    # Create an existing user
+    user = User.create(username: "Tartosso", email: "existing@example.com", password: "oldpassword")
+    user.confirm
+    id = user.id
+    visit "http://localhost:3000/users/sign_in"
+
+      fill_in "Email", with: "existing@example.com"
+      fill_in "Password", with: "oldpassword"
+      click_button "Login"
+
+    # Check for successful sign-in
+    expect(page).to have_content("Signed in successfully.")
+
+    # Navigate to the edit profile page
+    visit "http://localhost:3000/users/edit." + id.to_s
+
+    # Modify username and password
+    fill_in "Username", with: "NewUsername"
+    fill_in "user_password", with: "newpassword"
+    fill_in "user_password_confirmation", with: "newpassword"
+    click_button "Update"
+
+    # Check for successful update message
+    expect(page).to have_content("Your account has been updated successfully.")
+  end
+  ########## TEST N.3 INVALID CREDENTIALS ##########
 end
+
 
   # scenario "User can log in to their account" do
   #   user = User.create(email: "test@example.com", password: "password")
